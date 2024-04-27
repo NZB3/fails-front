d2fb0b8983d71868b79f09e9e0f14ee33abe35dd
Николай Бухалов <ni.bukhalov@gmail.com>
2024-04-27 11:41:32 +0300

    front

---
+++ index.js
@@ -0,0 +1,35 @@
+const serverAddress = "https://nzb3.github.io/without_fails_counter-back"
+const resetRoute = "fail"
+const getCountRoute = "/"
+
+async function getCount() {
+    fetch(`${serverAddress}${getCountRoute}`)
+        .then((response) => {
+            console.log(response)
+            return response.json()
+        })
+        .then(data => {
+            const counterDisplay = document.getElementsByClassName("count-display")[0];
+            counterDisplay.innerHTML = data.count
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+async function postReset() {
+    const response = await fetch(`${serverAddress}${resetRoute}`, {
+        method: "POST",
+        body: JSON.stringify({}),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+}
+
+async function reset() {
+    postReset()
+    getCount()
+}
+
+getCount()
